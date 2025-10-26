dd975a6b23f6c09a9a7e9b11f40583a2e3e5a754
Guibrick <guibertaina@hotmail.com>
2025-10-26 20:03:02 +0100

    Fix styling for different browsers

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { CssBaseline } from '@mui/material';
-import '@fontsource/montserrat/400.css';
-import '@fontsource/montserrat/500.css';
-import '@fontsource/montserrat/700.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "normalize.css";
+import "./index.css";
+import App from "./App";
+import { CssBaseline } from "@mui/material";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/700.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("No se encontró el elemento #root en el HTML.");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <CssBaseline />
     <App />
